b00238adeb42f4851fdedb2a1843d2c7371798f5
andycandy-de <68217449+andycandy-de@users.noreply.github.com>
2024-05-16 01:24:07 +0000

    Update zsh.nix

--- config/modules/zsh.nix
+++ config/modules/zsh.nix
@@ -12,29 +12,36 @@
 
   users.defaultUserShell = pkgs.zsh;
   users.users.root.shell = pkgs.bash;
-  
+
   programs.zsh = {
-  
+
     enable = true;
-    
+
     interactiveShellInit = with pkgs; ''
       HISTFILE=~/.histfile
       HISTSIZE=1000
       SAVEHIST=1000
+      EDITOR=hx
 
       source ${zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
       source ${zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       source ${zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
       source ${zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+      edit() {
+        test -w $1 -a -f $1 && $EDITOR $1 ||\
+           read -q "choice?No permissions to edit '$1'! Execute sudoedit [y/n]: " &&\
+           {echo ""; sudoedit $1} || echo ""
+      }
     '';
-    
+
     shellAliases = {
       fm = "nnn -dH && test -f ~/.config/nnn/.lastd && . ~/.config/nnn/.lastd; rm -f ~/.config/nnn/.lastd";
-      wm = "zellij";
+      wm = "zellij a || zellij";
       ls = "eza -a --color=always --long --no-filesize --icons=always --no-time --no-user --no-permissions";
       ll = "eza -a -1 --color=always --long --icons=always -g -h -U -m";
       cat = "bat --paging=never";
-      catp = "bat --paging=never -p";
+      catp = "bat -P -p";
     };
   };
 }
