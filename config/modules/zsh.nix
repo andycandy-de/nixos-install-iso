0795513e4aaef45cb55fb798f8bb66a1389ac925
andycandy-de <68217449+andycandy-de@users.noreply.github.com>
2024-05-21 00:24:52 +0000

    Update zsh.nix

--- config/modules/zsh.nix
+++ config/modules/zsh.nix
@@ -28,20 +28,102 @@
       source ${zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
       source ${zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-      edit() {
-        test -w $1 -a -f $1 && $EDITOR $1 ||\
-           read -q "choice?No permissions to edit '$1'! Execute sudoedit [y/n]: " &&\
-           {echo ""; sudoedit $1} || echo ""
+      _fn_test_rwx() {
+        local rwx=$1
+        local file=$2
+
+        if [[ $rwx =~ "r" ]] && [[ ! -r $file ]]; then
+          return 1
+        fi
+
+        if [[ $rwx =~ "w" ]] && [[ ! -w $file ]]; then
+          return 1
+        fi
+
+        if [[ $rwx =~ "x" ]] && [[ ! -x $file ]]; then
+          return 1
+        fi
+
+        return 0
       }
-    '';
+
+      _fn_open() {
+        local cmd_alias=$1
+        local permissions=$2
+        local command=$3
+        local file=$4
+
+        if [[ -z $file ]]; then
+          file=$(FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --type f" fzf --preview "bat -n --color=always --line-range :500 {}")
+        fi
+
+        if [[ -z $file ]]; then
+          return $?
+        fi
+
+        if [[ ! -f $file ]]; then
+          echo "The file '$file' does not exists!"
+          return 1
+        fi
+
+        if _fn_test_rwx $permissions $file; then
+          eval "$command $file"
+          return $?
+        else
+          echo "Permissions '$permissions' are required!"
+          read -q "choice?Execute '$cmd_alias $file' with sudo [y/n]: "
+          echo ""
+          if [[ $choice == "y" ]]; then
+            eval "sudo $command $file"
+            return $?
+          fi
+          return 1
+        fi
+      }
+
+      _edit() {
+        _arguments -C \
+          "1:file:_files"
+      }
+
+      edit() {
+        _fn_open "edit" "w" "$EDITOR" $1
+        return $?
+      }
+
+      _show() {
+        _arguments -C \
+          "1:file:_files"
+      }
+
+      show() {
+        _fn_open cat r 'bat --paging=never' $1
+        return $?
+      }
+
+      _shop() {
+        _arguments -C \
+          "1:file:_files"
+      }
+
+      shop() {
+        _fn_open cat r 'bat -P -p' $1
+        return $?
+      }
+
+      autoload -Uz compinit
+      compinit
+
+      compdef _edit edit
+      compdef _show show
+      compdef _shop shop
+      '';
 
     shellAliases = {
       fm = "nnn -dH && test -f ~/.config/nnn/.lastd && . ~/.config/nnn/.lastd; rm -f ~/.config/nnn/.lastd";
       wm = "zellij a || zellij";
       ls = "eza -a --color=always --long --no-filesize --icons=always --no-time --no-user --no-permissions";
       ll = "eza -a -1 --color=always --long --icons=always -g -h -U -m";
-      cat = "bat --paging=never";
-      catp = "bat -P -p";
     };
   };
 }
